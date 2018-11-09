b1e982612151bc96b775f717ee590005d3a53b29
David Buckley <buckley.w.david@gmail.com>
2018-11-09 12:17:37 -0500

    take messageID instead of actual message

--- buttons.go
+++ buttons.go
@@ -7,28 +7,28 @@ import (
 type Button struct {
 	Data     interface{}
 	Reaction string
-	Callback func(s *discordgo.Session, r *discordgo.MessageReactionAdd, m *discordgo.Message, data interface{})
+	Callback func(s *discordgo.Session, r *discordgo.MessageReactionAdd, mID string, cID string, data interface{})
 }
 
 // AddButton adds a listener for a specific reaction to a given message
-func AddButton(s *discordgo.Session, message *discordgo.Message, button Button, once bool) error {
+func AddButton(s *discordgo.Session, messageID string, channelID string, button Button, once bool) error {
 	var remove func()
 	f := func(sess *discordgo.Session, r *discordgo.MessageReactionAdd) {
 		// Reactions added by the bot do not count
 		// Reactions to other messages are irrelevant
 		// Reaction recieved needs to match reaction of the button
-		if r.UserID == s.State.User.ID || message.ID != r.MessageID || r.Emoji.Name != button.Reaction {
+		if r.UserID == s.State.User.ID || messageID != r.MessageID || channelID != r.ChannelID || r.Emoji.Name != button.Reaction {
 			return
 		}
 
-		button.Callback(s, r, message, button.Data)
+		button.Callback(s, r, messageID, channelID, button.Data)
 		if remove != nil && once {
 			remove()
 		}
 		return
 	}
 	remove = s.AddHandler(f)
-	s.MessageReactionAdd(message.ChannelID, message.ID, button.Reaction)
+	s.MessageReactionAdd(channelID, messageID, button.Reaction)
 
 	return nil
 }
